b32d8c531000fab2047372498403722ba616ccdb
lukasz <l.tyrala@gmail.com>
2024-03-22 18:25:03 +0100

    Unit 6 done and Astro tutorial finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://satellite-solstice.vercel.app"
+  site: "https://satellite-solstice.vercel.app",
+  integrations: [preact()]
 });
